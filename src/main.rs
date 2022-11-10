b8f8e63a3e06d1ed2e8edfd449ec95d2f0934450
Joris Hartog <jorishartog@hotmail.com>
2022-11-10 21:40:32 +0100

    🎺 Set working title

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use game_off_2022::GamePlugins;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Trumpet Trainee".to_string(),
+            ..default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugins(GamePlugins)
         .run();
