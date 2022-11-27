10171f2d56a844b8669996d1927fb46361e3a11c
Joris Hartog <jorishartog@hotmail.com>
2022-11-27 14:05:50 +0100

    🔧 Fix: set image sampling to nearest

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ use game_off_2022::GamePlugins;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                title: "Trumpet Trainee".to_string(),
-                ..default()
-            },
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "Trumpet Trainee".to_string(),
+                        resizable: false,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_plugins(GamePlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .run();
