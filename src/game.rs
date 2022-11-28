6bd9f7b3bb22652ed666927ee1c5becfac6ea35c
Joris Hartog <jorishartog@hotmail.com>
2022-11-29 00:11:13 +0100

    🏆 Add win screen

--- src/game.rs
+++ src/game.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 use std::time::Duration;
 
 use crate::level::Level;
+use crate::sprite::AnimationTimer;
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum GameState {
@@ -33,7 +34,7 @@ impl Plugin for GamePlugin {
             .add_system_set(SystemSet::on_update(GameState::GameOver).with_system(tick_state_timer))
             .add_system_set(SystemSet::on_enter(GameState::InGame).with_system(cleanup_volatile))
             .add_system_set(SystemSet::on_enter(GameState::Won).with_system(next_level))
-            .add_system_set(SystemSet::on_enter(GameState::Won).with_system(show_win_text))
+            .add_system_set(SystemSet::on_enter(GameState::Won).with_system(show_win_animation))
             .add_system_set(SystemSet::on_enter(GameState::Won).with_system(set_win_timer))
             .add_system_set(SystemSet::on_update(GameState::Won).with_system(tick_state_timer))
             .add_system_set(SystemSet::on_exit(GameState::Won).with_system(cleanup_volatile));
@@ -78,16 +79,26 @@ fn tick_state_timer(
     }
 }
 
-fn show_win_text(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn show_win_animation(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("sprites/elephant_tram_animation.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(128.0, 72.0), 8, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
     commands.spawn((
-        TextBundle::from_section(
-            "You've survived the day!",
-            TextStyle {
-                font: asset_server.load("fonts/PixeloidSans.ttf"),
-                font_size: 100.0,
-                color: Color::GREEN,
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform {
+                scale: Vec3::splat(10.0),
+                ..default()
             },
-        ),
+            ..default()
+        },
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
         Volatile,
     ));
 }
